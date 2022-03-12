5cc7821464e741edd9f57bb959ab9905fe04acf0
Chris Guest <chris.guest@trint.com>
2022-03-12 23:57:16 +0000

    Start an express example

---
+++ 03_simple_express/src/index.ts
@@ -0,0 +1,47 @@
+// src/app.ts
+import express from 'express'
+import pino from 'express-pino-logger'
+import bodyParser from 'body-parser'
+import { configureHoneycomb, shutdownHoneycomb } from './tracing'
+import * as dotenv from 'dotenv'
+import { logger } from './logger'
+
+function shutDown() {
+  return new Promise((resolve, reject) => {
+    shutdownHoneycomb()
+      .then(() => {
+        resolve('Complete')
+        process.exit(0)
+      })
+      .catch((e) => {
+        logger.error(e)
+        reject('Error')
+        process.exit(1)
+      })
+  })
+}
+
+export const app = express()
+dotenv.config()
+
+const apikey = process.env.HONEYCOMB_APIKEY ?? ''
+const dataset = process.env.HONEYCOMB_DATASET ?? ''
+const servicename = process.env.HONEYCOMB_SERVICENAME ?? ''
+configureHoneycomb(apikey, dataset, servicename)
+
+const port = process.env.PORT || 8000
+
+// Use body parser to read sent json payloads
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+)
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(pino())
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+process.on('SIGTERM', shutDown)
+process.on('SIGINT', shutDown)
---
+++ 03_simple_express/src/tracing.ts
@@ -0,0 +1,70 @@
+import { logger } from './logger'
+import process from 'process'
+import { Metadata, credentials } from '@grpc/grpc-js'
+import { NodeSDK } from '@opentelemetry/sdk-node'
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
+import { Resource } from '@opentelemetry/resources'
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc'
+import opentelemetry from '@opentelemetry/api'
+
+const metadata = new Metadata()
+let sdk: any = null
+
+export async function shutdownHoneycomb() {
+  logger.info('shutdownHoneycomb')
+  await sdk
+    .shutdown()
+    .then(() => logger.info('Tracing terminated'))
+    .catch((error: Error) => logger.error('Error terminating tracing', error))
+}
+
+export async function configureHoneycomb(apikey: string, dataset: string, servicename: string) {
+  logger.info(`'${servicename}' in '${dataset}' using '${apikey}'`)
+  metadata.set('x-honeycomb-team', apikey)
+  metadata.set('x-honeycomb-dataset', dataset)
+  const traceExporter = new OTLPTraceExporter({
+    url: 'grpc://api.honeycomb.io:443/',
+    credentials: credentials.createSsl(),
+    metadata,
+  })
+
+  sdk = new NodeSDK({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: servicename,
+    }),
+    traceExporter,
+    instrumentations: [
+      getNodeAutoInstrumentations({
+        // load custom configuration for http instrumentation
+        '@opentelemetry/instrumentation-http': {
+          applyCustomAttributesOnSpan: (span) => {
+            span.setAttribute('instrumentation-http', 'true')
+          },
+        },
+        '@opentelemetry/instrumentation-express': {},
+      }),
+    ],
+  })
+
+  await sdk
+    .start()
+    .then(() => {
+      logger.info('Tracing initialized')
+      const activeSpan = opentelemetry.trace
+        .getTracer('startup')
+        .startSpan('init', undefined, opentelemetry.context.active())
+      activeSpan?.addEvent('Tracing initialized', {})
+      activeSpan?.end()
+    })
+    .catch((error: Error) => logger.error('Error initializing tracing', error))
+
+  process.on('exit', shutdownHoneycomb)
+  process.on('SIGINT', shutdownHoneycomb)
+  process.on('SIGTERM', shutdownHoneycomb)
+  process.on('uncaughtException', async (error) => {
+    logger.error(error)
+    await shutdownHoneycomb()
+    process.exit(1)
+  })
+}

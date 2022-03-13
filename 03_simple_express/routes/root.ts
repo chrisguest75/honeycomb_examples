29c3c33d301a390550809d2bfc95db8e66a77745
Chris Guest <chris.guest@trint.com>
2022-03-13 00:36:28 +0000

    otel is not working yet.

--- 03_simple_express/routes/root.ts
+++ 03_simple_express/routes/root.ts
@@ -1,12 +1,15 @@
 import express, { Request, Response, NextFunction } from 'express'
 import { logger } from '../src/logger'
+import opentelemetry from '@opentelemetry/api'
 
 const router = express.Router()
 
 // use underscores to ignore parameters
 const rootHandler = (_request: Request, response: Response, _next: NextFunction) => {
-  logger.info('pingHandler')
+  const activeSpan = opentelemetry.trace.getSpan(opentelemetry.context.active())
+  logger.info('pingHandler' + activeSpan)
   response.status(200).json({ message: 'pong' })
+  activeSpan?.end()
 }
 
 router.get('/', rootHandler)

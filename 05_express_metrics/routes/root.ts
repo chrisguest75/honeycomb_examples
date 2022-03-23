99b78fe7b2fa06413f0c181569d61b9f331d0190
Chris Guest <chris.guest@trint.com>
2022-03-23 09:05:45 +0000

    Start a metrics examples

---
+++ 05_express_metrics/routes/root.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response, NextFunction } from 'express'
+import { logger } from '../src/logger'
+import opentelemetry from '@opentelemetry/api'
+
+const router = express.Router()
+
+// use underscores to ignore parameters
+const rootHandler = (_request: Request, response: Response, _next: NextFunction) => {
+  const activeSpan = opentelemetry.trace.getSpan(opentelemetry.context.active())
+  logger.info('rootHandler' + activeSpan)
+  activeSpan?.setAttribute('handler', 'rootHandler')
+  response.status(200).json({ message: 'pong', random: Math.floor(Math.random() * 100) })
+}
+
+router.get('/', rootHandler)
+
+export { router as rootRouter }

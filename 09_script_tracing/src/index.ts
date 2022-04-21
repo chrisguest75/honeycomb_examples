02e0a814a9125c1a4455bfb9d71029c131a25836
Chris Guest <chris.guest@trint.com>
2022-04-21 23:16:11 +0100

    Start a simple tool that will allow tracing scripts

---
+++ 09_script_tracing/src/index.ts
@@ -0,0 +1,64 @@
+import { logger } from './logger'
+import { configureHoneycomb, shutdownHoneycomb } from './tracing'
+import opentelemetry, { Span, SpanStatusCode } from '@opentelemetry/api'
+import * as dotenv from 'dotenv'
+
+// tracer for the file
+const tracerName = 'default'
+const tracer = opentelemetry.trace.getTracer(tracerName)
+
+export async function main(): Promise<string> {
+  // configure honeycomb
+  const apikey = process.env.HONEYCOMB_APIKEY ?? ''
+  const dataset = process.env.HONEYCOMB_DATASET ?? ''
+  const servicename = process.env.HONEYCOMB_SERVICENAME ?? ''
+  await configureHoneycomb(apikey, dataset, servicename)
+
+  // Create the root span for app
+  const activeSpan = tracer.startSpan('main')
+  if (activeSpan == undefined) {
+    logger.error('No active span')
+  }
+  // set parent span
+  opentelemetry.trace.setSpan(opentelemetry.context.active(), activeSpan)
+
+  activeSpan?.setAttribute('pino', `${logger.version}`)
+  logger.info(`Pino:${logger.version}`)
+  activeSpan?.addEvent(`Starting main`)
+
+  // create a span in the loop
+  const ctx = opentelemetry.trace.setSpan(opentelemetry.context.active(), activeSpan)
+  const workSpan = tracer.startSpan('doSomeWork', undefined, ctx)
+
+  //do some stuff
+
+  workSpan?.end()
+
+  activeSpan?.end()
+
+  return new Promise((resolve, reject) => {
+    logger.info('Attempting shutdown')
+    shutdownHoneycomb()
+      .then(() => {
+        resolve('Complete')
+      })
+      .catch((e) => {
+        logger.error(e)
+        reject('Error')
+      })
+  })
+}
+
+// load config
+dotenv.config()
+
+main()
+  .then(() => {
+    process.exit(0)
+  })
+  .catch((e) => {
+    logger.error(e)
+    process.exit(1)
+  })
+
+
---
+++ 09_script_tracing/src/tracing.ts
@@ -0,0 +1,80 @@
+import { logger } from './logger'
+import process from 'process'
+import { Metadata, credentials } from '@grpc/grpc-js'
+import { NodeSDK } from '@opentelemetry/sdk-node'
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
+import { Resource } from '@opentelemetry/resources'
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc'
+import opentelemetry, { DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api'
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http'
+
+const metadata = new Metadata()
+let sdk: any = null
+
+export async function shutdownHoneycomb() {
+  logger.info('shutdownHoneycomb')
+  await sdk
+    .shutdown()
+    .then(() => logger.info('Tracing terminated'))
+    .catch((error: Error) => logger.error('Error terminating tracing', error))
+}
+
+export async function configureHoneycomb(apikey: string, dataset: string, servicename: string) {
+  // configure otel diagnostics
+  const enableDiag = process.env.ENABLE_OTEL_DIAG ?? 'false'
+  if (enableDiag.toLowerCase() == 'true') {
+    opentelemetry.diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ALL)
+  }
+
+  logger.info(`'${servicename}' in '${dataset}' using '${apikey}'`)
+  metadata.set('x-honeycomb-team', apikey)
+  metadata.set('x-honeycomb-dataset', dataset)
+  const traceExporter = new OTLPTraceExporter({
+    url: 'grpc://api.honeycomb.io:443/',
+    credentials: credentials.createSsl(),
+    metadata,
+  })
+
+  const serviceversion = process.env.SERVICE_VERSION ?? 'unset'
+
+  sdk = new NodeSDK({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: servicename,
+      [SemanticResourceAttributes.SERVICE_VERSION]: serviceversion,
+    }),
+    traceExporter,
+    instrumentations: [
+      getNodeAutoInstrumentations({
+        // load custom configuration for http instrumentation
+        '@opentelemetry/instrumentation-http': {
+          applyCustomAttributesOnSpan: (span) => {
+            span.setAttribute('instrumentation-http', 'true')
+          },
+        },
+      }),
+      new HttpInstrumentation(),
+    ],
+  })
+
+  await sdk
+    .start()
+    .then(() => {
+      logger.info('Tracing initialized')
+      const activeSpan = opentelemetry.trace
+        .getTracer(servicename)
+        .startSpan('init', undefined, opentelemetry.context.active())
+      activeSpan?.addEvent('Tracing initialized', {})
+      activeSpan?.end()
+    })
+    .catch((error: Error) => logger.error('Error initializing tracing', error))
+
+  process.on('exit', shutdownHoneycomb)
+  process.on('SIGINT', shutdownHoneycomb)
+  process.on('SIGTERM', shutdownHoneycomb)
+  process.on('uncaughtException', async (error) => {
+    logger.error(error)
+    await shutdownHoneycomb()
+    process.exit(1)
+  })
+}

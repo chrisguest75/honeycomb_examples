6f1398d78ab55f347c47956b6f47043a48e94d7b
Chris Guest <chris.guest@trint.com>
2022-05-17 17:32:53 +0100

    More complex example. Downgrade Otel lib

--- 07_lambda_typescript_opentelemetry/README.md
+++ 07_lambda_typescript_opentelemetry/README.md
@@ -89,3 +89,6 @@ https://github.com/open-telemetry/opentelemetry-lambda/blob/main/nodejs/sample-a
 
 This is the wrapper with the callbacks
 https://github.com/open-telemetry/opentelemetry-lambda/blob/0a83149fe2f23a7dab64df6108cfa35f18cc2ae5/nodejs/packages/layer/src/wrapper.ts
+
+How to created nested spans without passing parents around
+https://github.com/open-telemetry/opentelemetry-js/issues/1963
--- 07_lambda_typescript_opentelemetry/src/functions/hello/handler.ts
+++ 07_lambda_typescript_opentelemetry/src/functions/hello/handler.ts
@@ -7,13 +7,11 @@ import { S3Client, ListBucketsCommand } from '@aws-sdk/client-s3';
 
 import schema from './schema';
 
-const tracerName = 'default'
-const tracer = opentelemetry.trace.getTracer(tracerName)
-
 // sleep for a period of time and create a child off passed in span
 function sleep(ms: number) {
-  // tracer for the file
-  const activeSpan = tracer.startSpan('sleep')
+  const activeSpan = opentelemetry.trace
+    .getTracer(process.env.OTEL_SERVICE_NAME)
+    .startSpan('sleep', undefined, opentelemetry.context.active())  
   activeSpan?.setAttribute('time', ms)
 
   return new Promise((resolve) => {
@@ -26,10 +24,54 @@ function sleep(ms: number) {
   })
 }
 
+function recursive_nested(times: number) {
+  const activeSpan = opentelemetry.trace
+    .getTracer(process.env.OTEL_SERVICE_NAME)
+    .startSpan('recursive_nested times', undefined, opentelemetry.context.active())  
+  activeSpan?.setAttribute('times', times)
+
+  opentelemetry.context.with(opentelemetry.trace.setSpan(opentelemetry.context.active(), activeSpan), () => {
+    sleep(Math.floor(Math.random() * 500))
+  })
+  
+  if (times == 0) {
+    logger.info(`Times ${times}`)
+    activeSpan?.end()
+    return 0
+  } else {
+    opentelemetry.context.with(opentelemetry.trace.setSpan(opentelemetry.context.active(), activeSpan), () => {
+      recursive_nested(times - 1)
+    })
+    activeSpan?.end()
+  }
+}
+
+function createResponse(name: string, numberBuckets: number, event: any) {
+  const activeSpan = opentelemetry.trace
+    .getTracer(process.env.OTEL_SERVICE_NAME)
+    .startSpan("formatJSONResponse", undefined, opentelemetry.context.active())
+  let response = formatJSONResponse({
+    message: `Hello ${name}, welcome to the exciting Serverless world!`,
+    numberOfBuckets: numberBuckets,
+    event,
+  });
+  activeSpan?.end()
+  return response;
+}
 
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  const sleeping = sleep(1000)
+  logger.info('Enter ValidatedEventAPIGatewayProxyEvent')
+  
+  const tracer = opentelemetry.trace.getTracer(process.env.OTEL_SERVICE_NAME)
+  const activeSpan = tracer.startSpan('ValidatedEventAPIGatewayProxyEvent', undefined, opentelemetry.context.active())
+  activeSpan?.addEvent('Enter ValidatedEventAPIGatewayProxyEvent', {})
 
+  let sleeping: Promise<unknown>
+  opentelemetry.context.with(opentelemetry.trace.setSpan(opentelemetry.context.active(), activeSpan), () => {
+    sleeping = sleep(1000)
+  })
+
+  const s3activeSpan = tracer.startSpan("s3stuff", undefined, opentelemetry.context.active())
   const client = new S3Client({
     region: 'us-east-1',
   });
@@ -39,20 +81,33 @@ const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) =
     function: 'hello',
     numberOfBuckets: s3response.Buckets?.length,
   });
+  s3activeSpan?.end()
 
+  // TODO: typing?
+  let response
+  opentelemetry.context.with(opentelemetry.trace.setSpan(opentelemetry.context.active(), activeSpan), () => {
+    response = createResponse(event.body.name, s3response.Buckets?.length, event) 
+  })
 
-  let response = formatJSONResponse({
-    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
-    event,
-    numberOfBuckets: s3response.Buckets?.length,
-  });
+  let sleeping2: Promise<unknown>
+  opentelemetry.context.with(opentelemetry.trace.setSpan(opentelemetry.context.active(), activeSpan), () => {
+    sleeping2 = sleep(500)
+  })
 
+  opentelemetry.context.with(opentelemetry.trace.setSpan(opentelemetry.context.active(), activeSpan), () => {
+    recursive_nested(5) 
+  })
+ 
   await sleeping
+  await sleeping2
   
+  activeSpan?.end()
+  logger.info('Exit ValidatedEventAPIGatewayProxyEvent')
   return response
 };
 
 // NOTE: https://github.com/aws-observability/aws-otel-lambda/issues/99
 const main = middyfy(hello);
+
 
 module.exports = { main };

ac62e29c1ebe0b0a33314eea075985b27906fc28
Chris Guest <chris.guest@trint.com>
2022-05-18 22:22:35 +0100

    Update plugins

--- 07_lambda_typescript_opentelemetry/plugins.js
+++ 07_lambda_typescript_opentelemetry/plugins.js
@@ -5,5 +5,12 @@ const { nodeExternalsPlugin } = require('esbuild-node-externals');
 // default export should be an array of plugins
 module.exports = [nodeExternalsPlugin({
     packagePath: './package.json',
+    allowList: [
+        /*"@middy/core",
+        "@middy/http-json-body-parser",
+        "@aws-sdk/client-s3",
+        "@opentelemetry/api",
+        "pino",*/
+    ]
 })];
 

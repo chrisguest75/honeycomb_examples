cfba0d86f39c9edf942425259d92d8e6594efba9
Christopher Guest <chrisguest75@users.noreply.github.com>
2022-05-27 08:45:42 +0100

    Start adding basics for no collector (#18)
    * Start adding basics for no collector
    
    * Trying to troubleshoot gprc.
    
    * Working wihout a collector
    
    Co-authored-by: Chris Guest <chris.guest@trint.com>

--- 10_script_tracing/README.md
+++ 10_script_tracing/README.md
@@ -4,7 +4,6 @@ Demonstrate using tracing `equinix-labs/otel-cli`
 
 TODO:
 
-* Is it possible without the collector?
 * use tp-export.
 * handle errors
 * events
@@ -29,14 +28,42 @@ cp ./.env.template ./.env
 ./control.sh --profile=all --stop
 ```
 
-## Start and test local
+## Start and test (local script)
 
 ```sh
 # start otel collector
-./control.sh --profile=collector --start 
+./control.sh --profile=collectoronly --start 
 
 # run test local 
 ./client/test-cli.sh            
+# ensure all stopped
+./control.sh --profile=collectoronly --stop
+
+# now without collector
+./client/test-cli-direct.sh
+```
+
+## Start and test (containerised script)
+
+```sh
+# start otel collector and tracing container
+./control.sh --profile=collector --start 
+
+# exec into it
+docker compose -f docker-compose-with-collector.yaml --profile all exec -it tracing /bin/bash
+```
+
+## Start and test (direct no collector)
+
+```sh
+# send tracing direct
+./client/test-cli-direct.sh
+```
+
+
+```sh
+# debugging
+otel-cli status --config ./config.json  
 ```
 
 ## Resources
@@ -46,3 +73,4 @@ cp ./.env.template ./.env
 * OpenTelemetry Collector Demo [here](https://github.com/open-telemetry/opentelemetry-collector-contrib/tree/main/examples/demo)  
 * Honeycomb OpenTelemetry Collector [here](https://docs.honeycomb.io/getting-data-in/otel-collector/)  
 * Download Releases from Github using Curl and Wget [here](https://dev.to/saintdle/download-releases-from-github-using-curl-and-wget-54fi)  
+https://github.com/grpc/grpc/blob/master/TROUBLESHOOTING.md
---
+++ 10_script_tracing/client/test-cli-direct.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+set -euf -o pipefail
+
+readonly SCRIPT_NAME=$(basename "$0")
+readonly SCRIPT_PATH=${0}
+readonly SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+readonly HOME_DIR=~
+if [[ $(command -v greadlink) ]]; then 
+    # mac requires 'brew install coreutils'
+    readonly SCRIPT_FULL_PATH="$(dirname "$(greadlink -f "$0")")"
+else
+    readonly SCRIPT_FULL_PATH="$(dirname "$(readlink -f "$0")")"
+fi 
+
+if [ -n "${DEBUG_ENVIRONMENT-}" ];then 
+    # if DEBUG_ENVIRONMENT is set
+    env
+    export
+fi
+
+ENVFILE="./.env"
+if [ -f "${ENVFILE}" ]; then
+    echo "Source ${ENVFILE}"
+    . "${ENVFILE}"
+fi
+
+function span_function {
+    echo "**** $FUNCNAME NumArgs:$# ****"
+    local work_function=$1
+    local span_name=$2
+    local cliout=
+
+    #export GODEBUG=http2debug=1
+    #export GODEBUG=http2debug=2 
+    #export GRPC_TRACE=all
+    #export GRPC_VERBOSITY=DEBUG 
+    #export GRPC_TRACE=list_tracers
+
+    #export OTEL_EXPORTER_OTLP_ENDPOINT="${HONEYCOMB_APIHOST}"
+    #export OTEL_EXPORTER_OTLP_HEADERS="x-honeycomb-team=${HONEYCOMB_APIKEY}"
+    #echo "OTEL_EXPORTER_OTLP_ENDPOINT=$OTEL_EXPORTER_OTLP_ENDPOINT"
+    #echo "OTEL_EXPORTER_OTLP_HEADERS=$OTEL_EXPORTER_OTLP_HEADERS"
+    local start=$($DATE_COMMAND --rfc-3339=ns) # rfc3339 with nanoseconds
+    eval "$work_function"
+    local end=$($DATE_COMMAND +%s.%N) # Unix epoch with nanoseconds
+    if [[ $# -ge 3 ]]; then
+        local attributes=$3
+        #--endpoint "https://api.honeycomb.io" --otlp-headers "x-honeycomb-team=wUfkaOpW1HWce2pT9Sw27I"
+        cliout=$(otel-cli span --config ${SCRIPT_FULL_PATH}/config.json --fail --verbose --tp-print --insecure true -n "${HONEYCOMB_SERVICENAME}" -s "${span_name}" --start "$start" --end "$end" --attrs "$attributes")
+    else
+        cliout=$(otel-cli span --config ${SCRIPT_FULL_PATH}/config.json --fail --verbose --tp-print --insecure true -n "${HONEYCOMB_SERVICENAME}" -s "${span_name}" --start "$start" --end "$end")
+    fi
+    #echo "$cliout"
+    cliout=$(echo "$cliout" | grep "TRACEPARENT")
+    #echo "$cliout"
+    eval "export $cliout"
+    echo "TRACEPARENT=$TRACEPARENT"
+}
+
+function root {
+    echo "**** $FUNCNAME NumArgs:$# ****"
+}
+
+function copy_s3 {
+    echo "**** $FUNCNAME NumArgs:$# ****"
+    local src_file=$1
+    local dst_file=$2
+    local filesize="$(( ( RANDOM % 10 )  + 1 ))"
+    echo "copying '$src_file' to '$dst_file' - size $filesize mb"
+    sleep $filesize
+}
+
+echo "***************************************"
+echo "** Generate spans"
+echo "***************************************"
+otel-cli status --config ${SCRIPT_FULL_PATH}/config.json
+
+# root span
+SPAN_NAME=root
+span_function "${SPAN_NAME}" "processing files (no collector)" "entrypoint='true',language='gb'"
+root_store="$TRACEPARENT"
+echo "ROOTSPAN=$root_store"
+
+SPAN_NAME=s3_copy
+SRC_FILE="s3://file.txt"
+DST_FILE="./file.txt"
+span_function "copy_s3 '$SRC_FILE' '$DST_FILE'" "${SPAN_NAME}" "attribute1='test value',attribute2='is this working?',src_file=$SRC_FILE,dst_file=$DST_FILE"
+
+
+unset TRACEPARENT

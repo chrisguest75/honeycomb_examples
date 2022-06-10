5a89f0e86073c4815f145435565b54719c3db9bd
Chris Guest <chris.guest@trint.com>
2022-06-10 16:28:30 +0100

    Add more complex handlers for ping

--- 05_express_metrics/server/routes/ping.ts
+++ 05_express_metrics/server/routes/ping.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response, NextFunction } from 'express'
 import { logger } from '../src/logger'
-import opentelemetry from '@opentelemetry/api'
+import opentelemetry, { SpanStatusCode } from '@opentelemetry/api'
 import promClient from 'prom-client'
 const pong_counter = new promClient.Counter({
   name: 'test_prometheus_pongs_generated',
   help: 'Number of pongs generated',
+})
+const pong_guage = new promClient.Gauge({
+  name: 'test_prometheus_random_value',
+  help: 'Random value',
 })
 
 const router = express.Router()
@@ -19,8 +23,17 @@ const pingHandler = async (_request: Request, response: Response, _next: NextFun
   activeSpan?.setAttribute('handler', 'pingHandler')
 
   pong_counter.inc(1)
+  const random = Math.floor(Math.random() * 100)
+  pong_guage.set(random)
+  activeSpan.setStatus({ code: SpanStatusCode.OK })
+  let status = 200
+  if (random % 42 == 0) {
+    status = 503
+    activeSpan?.addEvent('Divisible by 42', { random: random })
+    activeSpan.setStatus({ code: SpanStatusCode.ERROR })
+  }
 
-  response.status(200).json({ message: 'pong', random: Math.floor(Math.random() * 100) })
+  response.status(status).json({ message: 'pong', random: random })
   activeSpan?.end()
 }
 
